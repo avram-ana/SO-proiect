a839e97024ebe6082e59dc0c4828d1db704bb39c
avram-ana <ana7.avram@gmail.com>
2025-04-15 14:15:00 +0300

    Added function for removing given directory and its associated symlink

--- SO_proiect.c
+++ SO_proiect.c
@@ -8,6 +8,7 @@
 #include <fcntl.h>
 #include <stdbool.h>
 #include <time.h>
+#include <dirent.h>
 
 #define MAX 500
 
@@ -33,6 +34,72 @@ long get_file_size(char *filename)
   
   return file_status.st_size;
 }
+
+void remove_dir(const char *hunt_name)
+{
+  DIR *dir;
+  struct dirent *entry;
+  char full_path[200];
+
+  // does the directory exist?
+  struct stat st;
+  if(stat(hunt_name, &st) == -1 || !S_ISDIR(st.st_mode))  // S_ISDIR - checks whether it's a dir or not
+    {
+      const char *msg = "Error: Hunt directory doesn't exist.\n";
+      write(STDOUT_FILENO, msg, strlen(msg));
+      return;
+    }
+
+  if(!(dir = opendir(hunt_name)))
+    {
+      perror("Error: Could not open hunt directory");
+      exit(-1);
+    }
+
+  // create path for symlink:
+  char log_symlink[200];
+  strcpy(log_symlink, "logged_hunt-");
+  strcat(log_symlink, hunt_name);
+
+  // remove symbolic link:
+  if(lstat(log_symlink, &st) == 0 && S_ISLNK(st.st_mode))
+    {
+      if(unlink(log_symlink) != 0)  // unlink "unlinks" the dir before deleting (remove symlink)
+        {
+	  perror("Error removing symbolic link");
+	  exit(-1);
+        }
+    }
+  
+  // remove the content from given dir
+  while((entry = readdir(dir)) != NULL)
+    {
+      if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+        continue;
+
+      strcpy(full_path, hunt_name);
+      strcat(full_path, "/");
+      strcat(full_path, entry->d_name);
+
+      if(remove(full_path) != 0)
+        {
+          printf("Couldn't remove a file.\n");
+	  exit(-1);
+        }
+    }
+
+  closedir(dir);
+
+  // remove the given dir
+  if (remove(hunt_name) != 0)
+    {
+      const char *fail = "Error: Could not remove the hunt directory itself.\n";
+      write(STDOUT_FILENO, fail, strlen(fail));
+      return;
+    }
+}
+
+
 
 void add_hunt(const char *id)  // argv: something like hunt001
 {
@@ -102,14 +169,42 @@ void add_hunt(const char *id)  // argv: something like hunt001
 
   // write info in binary file:
 
-  write(f, new_treasure.id, strlen(new_treasure.id));
-  write(f, new_treasure.nume, strlen(new_treasure.nume));
-  write(f, new_treasure.latitude, strlen(new_treasure.latitude));
-  write(f, new_treasure.longitude, strlen(new_treasure.longitude));
-  write(f, new_treasure.clue, strlen(new_treasure.clue));
-  write(f, new_treasure.value, strlen(new_treasure.value));
-  write(f, "\n", 1);
-
+  if(write(f, new_treasure.id, strlen(new_treasure.id)) == -1)
+    {
+      perror("Error writing ID in new binary_file");
+      exit(-1);
+    }
+  if(write(f, new_treasure.nume, strlen(new_treasure.nume)) == -1)
+    {
+      perror("Error writing name in new binary_file");
+      exit(-1);
+    }
+  if(write(f, new_treasure.latitude, strlen(new_treasure.latitude)) == -1)
+    {
+      perror("Error writing latitude in new binary_file");
+      exit(-1);
+    }
+  if(write(f, new_treasure.longitude, strlen(new_treasure.longitude)) == -1)
+    {
+      perror("Error writing longitude in new binary_file");
+      exit(-1);
+    }
+  if(write(f, new_treasure.clue, strlen(new_treasure.clue)) == -1)
+    {
+      perror("Error writing clue in new binary_file");
+      exit(-1);
+    }
+  if(write(f, new_treasure.value, strlen(new_treasure.value)) == -1)
+    {
+      perror("Error writing value in new binary_file");
+      exit(-1);
+    }
+  if(write(f, "\n", 1) == -1)
+    {
+      perror("Error writing *backslash n* in new binary_file");
+      exit(-1);
+    }
+      
   close(f);  // done!!!
 
   //__________________________________
@@ -394,6 +489,10 @@ int main(int argc, char **argv)
 	  printf("For this you need argv[4].\n");
 	  exit(-1);
 	}
+    }
+  else if(strcmp(argv[1], "--removeDir") == 0)
+    {
+      remove_dir(argv[2]);
     }
   
   return 0;
