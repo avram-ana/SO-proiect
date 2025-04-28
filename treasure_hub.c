4685c1d48f1a8bc24ca096dd52c14d139c050007
avram-ana <ana7.avram@gmail.com>
2025-04-28 17:46:50 +0300

    Create treasure_hub.c
    Besides start_monitor, stop_monitor and exit, only the list_treasures function is implemented.

---
+++ treasure_hub.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+pid_t monitor_pid = -1;
+int monitor_stopped = 1;  // 1 - monitor is stopped | 0 - monitor running
+
+void print_details(void)
+{
+  printf("\n___TREASURE HUB COMMANDS___\n");
+  printf("- start_monitor  // starts a separate background process\n");
+  printf("- list_hunts  // asks the monitor to list the hunts and the total number of treasures in each\n");  // not implemented yet!!!!!!!!!!!!!!!!!!!!!!
+  printf("- list_treasures  // tells the monitor to show the information about all treasures in a hunt\n");
+  printf("- view_treasure  // tells the monitor to show the information about a treasure in hunt\n");  // not implemented yet!!!!!!!!!!!!!
+  printf("- stop_monitor  // asks the monitor to end then returns to the prompt\n");
+  printf("- exit  // if the monitor still runs, prints an error message, otherwise ends the program\n\n");
+}
+
+void sigchild_handler(int sig)
+{
+  // handles the end of the monitor process
+  // (or child process) and updates the status.
+  (void)sig;
+  int status;
+  pid_t pid;
+  
+  while ((pid = waitpid(-1, &status, WNOHANG)) > 0)
+    {
+      if (pid == monitor_pid)
+        {
+	  printf("[hub] monitor terminated with status %d\n", WEXITSTATUS(status));
+	  // change status to "stopped monitor":
+	  monitor_pid = -1;
+	  monitor_stopped = 1;
+        }
+    }
+}
+
+void start_monitor(void)
+{
+  // if the monitor is already running we stop:
+  if (monitor_pid != -1)
+    {
+      printf("[hub] Monitor is already running with PID %d.\n", monitor_pid);
+      return;
+    }
+
+  // else, actually start the monitor:
+
+  //create a child process
+  pid_t pid = fork();
+  if(pid == -1)
+    {
+      perror("Error with the fork");
+      exit(-1);
+    }
+  else if(pid == 0)  // we are inside the child process:
+    {
+      while(1)
+        {
+	  pause();  // the process waits for signals
+        }
+    }
+  else  // we are inside the parent process:
+    {
+      monitor_pid = pid;
+      // set status to "monitor running":
+      monitor_stopped = 0;
+      printf("[hub] Monitor started with PID %d.\n", pid);
+    }
+}
+
+void stop_monitor(void)
+{
+  // can't stop a "stopped" monitor
+  if (monitor_pid == -1 || monitor_stopped == 1)
+    {
+      printf("[hub] No monitor running.\n");
+      return;
+    }
+  // send  kill signal to stop monitor:
+  kill(monitor_pid, SIGKILL);
+
+  // change status:
+  monitor_stopped = 1;
+  monitor_pid = -1;
+  printf("[hub] Monitor stopped.\n");
+}
+
+void execute_list_treasures(void)
+{
+  char hunt_id[20];
+  printf("[hub] Enter the ID for the desired hunt: ");
+  fgets(hunt_id, sizeof(hunt_id), stdin);
+  hunt_id[strcspn(hunt_id, "\n")] = 0;  // Remove the newline character
+  
+  char command[300];
+  // treasure_manager.c requires strict args: --list for this function and the ID for the desired hunt:
+  snprintf(command, sizeof(command), "./treasure_manager --list %s", hunt_id);
+
+  // system - if a child process could not be created, or its status could not be retrieved,
+  // the return value is -1 and errno is set to indicate the error.
+  if (system(command) == -1)
+    {
+      perror("Error executing treasure_manager");
+      exit(-1);
+    }
+}
+
+void process_command(const char *input)
+{
+  if(strcmp(input, "list_treasures") == 0)
+    {
+      if(monitor_pid != -1 && monitor_stopped == 0)
+        {
+	  execute_list_treasures();
+        }
+      else
+        {
+	  printf("[hub] cannot execute command, no monitor running.\n");
+        }
+    }
+  else if(strcmp(input, "start_monitor") == 0)
+    {
+      start_monitor();
+    }
+  else if(strcmp(input, "stop_monitor") == 0)
+    {
+      stop_monitor();
+    }
+  else if(strcmp(input, "exit") == 0)
+    {
+      printf("[hub] Exiting...\n");
+      exit(0);
+    }
+  else
+    {
+      printf("[hub] Unknown command!\n");
+    }
+}
+
+int main(void)
+{
+  print_details();
+  
+  struct sigaction sa;
+  sa.sa_handler = sigchild_handler;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  
+  if(sigaction(SIGCHLD, &sa, NULL) == -1)
+    {
+      perror("Error with sigaction");
+      exit(-1);
+    }
+  
+  char input[200];
+  
+  while(1)
+    {
+      printf("[hub] > ");
+      fflush(stdout);
+      
+      if(fgets(input, sizeof(input), stdin) == NULL)
+        {
+	  printf("\n");
+	  break;
+        }
+      
+      input[strcspn(input, "\n")] = 0; // remove newline character
+      
+      process_command(input);
+    }
+  
+  return 0;
+}
